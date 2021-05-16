45e7e369b4c938f113c8d9a8064117d5f57122fb
bshantherishenoy <63908336+bshantherishenoy@users.noreply.github.com>
2021-05-16 12:43:24 +0530

    example for Acchu

--- main.py
+++ main.py
@@ -10,18 +10,20 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
-timer=None
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
 def reset_timer():
     window.after_cancel(timer)
-    label1.config(text="Timer",fg=GREEN)
+    label1.config(text="Timer", fg=GREEN)
     canvas.itemconfig(timer_text, text="00:00")
     label2.config(text=None)
     global reps
-    reps=0
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+
 def start_timer():
     global reps
     work_sec = WORK_MIN * 60
@@ -48,25 +50,29 @@ def start_timer():
 
 
 
-#---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
 def count_down(count):
     count_min = math.floor(count / 60)
     count_seconds = count % 60
     if count_seconds == 0:
-        count_seconds ="00" # Dynamic typing of python
+        count_seconds = "00" # Dynamic typing of python
     elif count_seconds % 10 == count_seconds:
         count_seconds = f"0{count_seconds}"
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_seconds}")
-    if count>0:
+    if count > 0:
         global timer
-        timer=window.after(1000, count_down, count-1)
+        timer = window.after(1000, count_down, count-1)
     else:
         start_timer()
         mark = ""
         for _ in range(math.floor(reps/2)):
-            mark+="✔"
+            mark += "✔"
         label2.config(text=mark)
 # ---------------------------- UI SETUP ------------------------------- #
+
+
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
@@ -82,7 +88,7 @@ timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT
 
 canvas.grid(row=2, column=2)
 
-button1 = Button(text="Start", highlightthickness=0,command=start_timer)
+button1 = Button(text="Start", highlightthickness=0, command=start_timer)
 button1.grid(row=3, column=1)
 
 button2 = Button(text="Reset", highlightthickness=0, command=reset_timer)
